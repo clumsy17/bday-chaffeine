95e30a995f1e802efd7ee395ce72b10fd19cab13
clumsy17 <rezasaputranewv1@gmail.com>
2026-09-14 00:57:36 +0700

    Update login.js

--- api/login.js
+++ api/login.js
@@ -34,7 +34,7 @@ module.exports = (req, res) => {
   const password = String(body.password || '');
 
   if (username !== USERNAME || password !== PASSWORD) {
-    res.status(401).json({ ok: false, error: 'Salah Ih Jangan Maksa :p' });
+    res.status(401).json({ ok: false, error: 'Salah ih jangan maksa :p' });
     return;
   }
 
